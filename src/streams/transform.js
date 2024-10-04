aa5f69724e90b5c8b304a2296d15828e5d5e51f7
Sergey <seygorin@gmail.com>
2024-10-04 19:41:37 +0500

    feat: streams tasks completed

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
-const transform = async () => {
-    // Write your code here 
-};
+import {Transform} from 'stream'
 
-await transform();
+const reverseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    const text = chunk.toString().trim()
+    if (text) {
+      const reversed = text.split('').reverse().join('')
+      this.push(reversed + '\n')
+    }
+    callback()
+  },
+})
+
+const transform = async () => {
+  console.log('Enter text to reverse. Press Ctrl+C to exit:')
+
+  process.stdin.setEncoding('utf8')
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout)
+
+  return new Promise((resolve) => {
+    process.on('SIGINT', () => {
+      console.log('\nTransform stream closed')
+      resolve()
+      process.exit()
+    })
+  })
+}
+
+await transform()
