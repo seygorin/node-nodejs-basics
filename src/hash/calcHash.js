5ac96009367004a49f4687f103d3dbd547e1655b
Sergey <seygorin@gmail.com>
2024-10-04 19:17:08 +0500

    feat: hash tasks completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import {createHash} from 'crypto'
+import {createReadStream} from 'fs'
+import {fileURLToPath} from 'url'
+import path from 'path'
 
-await calculateHash();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const calculateHash = async () => {
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+  const hash = createHash('sha256')
+
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(filePath)
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk)
+    })
+
+    stream.on('end', () => {
+      const fileHash = hash.digest('hex')
+      console.log(fileHash)
+      resolve(fileHash)
+    })
+
+    stream.on('error', (error) => {
+      reject(error)
+    })
+  })
+}
+
+await calculateHash()
